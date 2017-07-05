eb38e733bfe14d7be57ed660bf0cd99804affdbb
Helioagfo <Helio.agfo@gmail.com>
2017-07-05 15:59:46 -0500

    1.2 PANTALLA ACTUALIZA XSOADDRESS, FALTA QUE RESIVA PARAMETROS

--- AU25110/Form1.Designer.vb
+++ AU25110/Form1.Designer.vb
@@ -29,34 +29,129 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
-        Me.Update1 = New Interop.SAF.SAFUpdate
-        Me.SAFHelpProvider = New System.Windows.Forms.HelpProvider
+        Me.Update1 = New Interop.SAF.SAFUpdate()
+        Me.SAFHelpProvider = New System.Windows.Forms.HelpProvider()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.cBanco = New Interop.SAF.SAFCombo()
+        Me.cMetodoPago = New Interop.SAF.SAFCombo()
+        Me.cDigitosCuenta = New Interop.SAF.SAFMaskedText()
         CType(Me.Update1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox1.SuspendLayout()
+        CType(Me.cBanco, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.cMetodoPago, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.cDigitosCuenta, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'Update1
         '
         Me.Update1.Enabled = True
-        Me.Update1.Location = New System.Drawing.Point(220, 238)
+        Me.Update1.Location = New System.Drawing.Point(243, 123)
+        Me.Update1.Margin = New System.Windows.Forms.Padding(4)
         Me.Update1.Name = "Update1"
         Me.Update1.OcxState = CType(resources.GetObject("Update1.OcxState"), System.Windows.Forms.AxHost.State)
-        Me.Update1.Size = New System.Drawing.Size(26, 26)
+        Me.Update1.Size = New System.Drawing.Size(33, 33)
         Me.Update1.TabIndex = 0
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.cDigitosCuenta)
+        Me.GroupBox1.Controls.Add(Me.cMetodoPago)
+        Me.GroupBox1.Controls.Add(Me.cBanco)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(623, 187)
+        Me.GroupBox1.TabIndex = 1
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Datos Bancarios"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(30, 110)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(166, 17)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Últimos 4 Dígitos Cuenta:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(30, 80)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(96, 17)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Metodo Pago:"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(30, 50)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(52, 17)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Banco:"
+        '
+        'cBanco
+        '
+        Me.cBanco.Location = New System.Drawing.Point(197, 35)
+        Me.cBanco.Name = "cBanco"
+        Me.cBanco.OcxState = CType(resources.GetObject("cBanco.OcxState"), System.Windows.Forms.AxHost.State)
+        Me.cBanco.Size = New System.Drawing.Size(260, 24)
+        Me.cBanco.TabIndex = 6
+        '
+        'cMetodoPago
+        '
+        Me.cMetodoPago.Location = New System.Drawing.Point(197, 55)
+        Me.cMetodoPago.Name = "cMetodoPago"
+        Me.cMetodoPago.OcxState = CType(resources.GetObject("cMetodoPago.OcxState"), System.Windows.Forms.AxHost.State)
+        Me.cMetodoPago.Size = New System.Drawing.Size(260, 24)
+        Me.cMetodoPago.TabIndex = 7
+        '
+        'cDigitosCuenta
+        '
+        Me.cDigitosCuenta.Location = New System.Drawing.Point(197, 80)
+        Me.cDigitosCuenta.Name = "cDigitosCuenta"
+        Me.cDigitosCuenta.OcxState = CType(resources.GetObject("cDigitosCuenta.OcxState"), System.Windows.Forms.AxHost.State)
+        Me.cDigitosCuenta.Size = New System.Drawing.Size(201, 24)
+        Me.cDigitosCuenta.TabIndex = 8
         '
         'Form1
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(292, 273)
+        Me.ClientSize = New System.Drawing.Size(647, 210)
+        Me.Controls.Add(Me.GroupBox1)
         Me.Controls.Add(Me.Update1)
+        Me.Margin = New System.Windows.Forms.Padding(4)
         Me.Name = "Form1"
-        Me.Text = "Form1"
+        Me.Text = "Datos Bancarios (AU.251.10)"
         CType(Me.Update1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        CType(Me.cBanco, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.cMetodoPago, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.cDigitosCuenta, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
     Friend WithEvents Update1 As Interop.SAF.SAFUpdate
     Friend WithEvents SAFHelpProvider As System.Windows.Forms.HelpProvider
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents cBanco As Interop.SAF.SAFCombo
+    Friend WithEvents cMetodoPago As Interop.SAF.SAFCombo
+    Friend WithEvents cDigitosCuenta As Interop.SAF.SAFMaskedText
 
 End Class
--- AU25110/Form1.vb
+++ AU25110/Form1.vb
@@ -22,29 +22,19 @@ Friend Class Form1
 
     Private Sub Form1_Load(ByVal eventSender As System.Object, ByVal eventArgs As System.EventArgs) Handles MyBase.Load
 
-        ' Load all the forms needed for this application
-        'Call LoadForm(fSL01001)
-
-        ' Initialize the application as a Microsoft Dynamics SL Application
+      
         Call ApplInit()
+        Call Init_xSoAddress(LEVEL0, True)
 
-
-        ' Call Set Address for the tables that will have fields displayed on the scree,
-        ' or that you would like customization manager to be able to use
-        'Call SetAddr(LEVEL0, "bxSLSample", bxSLSample, nxSLSample)
-
-        ' Define the cursors that are used by the application
-
-        ' This is an example for a table in the System Database
-        'Call SqlCursor(c1, LEVEL0 + SqlSystemDb)
-
-        ' This is an example for a table in an Application Database
-        'Call SqlCursor(c1, LEVEL0 )
-
-
-        ' Call the screen init function
         Call ScreenInit()
 
+        Call InitBuffer()
+
+        With Timer1
+            .Interval = 10000
+            .Enabled = True
+            .Start()
+        End With
 
 
 
@@ -54,6 +44,151 @@ Friend Class Form1
 
         'Standard Screen Exit Call
         Call ScreenExit("", "")
+
+    End Sub
+
+    Private Sub GroupBox1_Enter(sender As Object, e As EventArgs) Handles GroupBox1.Enter
+
+    End Sub
+
+    Private Sub Update1_UpdateEvent(Level As Short, InsertFlg As Short, LevelsDone As Short, LevelsLeft As Short, ByRef RetVal As Short) Handles Update1.UpdateEvent
+        Dim Custid As String
+        Dim ShiptoId As String
+        Dim Query As String
+        Stop
+        Custid = "000000"
+        ShiptoId = "000030"
+
+        If Level = LEVEL0 Then
+            Query = "Select * from xsoaddress where custid = " + SParm(RTrim(Custid)) + "and shiptoid =" + SParm(RTrim(ShiptoId))
+            Call sql(CSR_xSoAddress, Query)
+            serr1 = SFetch1(CSR_xSoAddress, bxSoAddress2)
+
+            If serr1 = NOTFOUND Then
+                Try
+                    'Call TranBeg(True)
+                    'Call SInsert1(CSR_xPld, "xPld", bxPld) 'Inserta
+                Catch ex As Exception
+                    TranAbort()
+                Finally
+                    TranEnd()
+                End Try
+            Else
+                Try
+                    Call TranBeg(True)
+                    bxSoAddress2.Banco = bxSoAddress.Banco
+                    bxSoAddress2.MetodoPago = bxSoAddress.MetodoPago
+                    bxSoAddress2.DigitosCuenta = bxSoAddress.DigitosCuenta
+
+                    Call SUpdate1(CSR_xSoAddress, "xsoAddress", bxSoAddress2) 'Actualiza
+                Catch ex As Exception
+                    TranAbort()
+                Finally
+                    TranEnd()
+                End Try
+            End If
+            RetVal = NoAction
+
+        End If
+
+    End Sub
+
+    Private Sub InitBuffer()
+
+        With bxSoAddress
+            .[Addr1] = " "
+            .[Addr2] = " "
+            .[ApellidoMaterno] = " "
+            .[ApellidoPaterno] = " "
+            .[Attn] = " "
+            .[Banco] = " "
+            .[City] = " "
+            .[Ciudad] = " "
+            .[Clave] = " "
+            .[COGSAcct] = " "
+            .[COGSSub] = " "
+            .[Colonia] = " "
+            .[Country] = " "
+            .Crtd_DateTime = bpes.Today
+            .Crtd_Prog = bpes.ScrnNbr
+            .Crtd_User = bpes.UserId
+            .[CURP] = " "
+            .[CustId] = " "
+            .[Descr] = " "
+            .[DigitosCuenta] = " "
+            .[DiscAcct] = " "
+            .[DiscSub] = " "
+            .[Domicilio] = " "
+            .[EmailAddr] = " "
+            .[Escolaridad] = " "
+            .[EstadoCivil] = " "
+            .[Fax] = " "
+            .[FechaNacimiento] = 0
+            .[FOB] = " "
+            .[FrghtCode] = " "
+            .[FrtAcct] = " "
+            .[FrtSub] = " "
+            .[FrtTermsID] = " "
+            .[GeoCode] = " "
+            .[LadaCasa] = " "
+            .[LadaOficina] = " "
+            .[Lupd_DateTime] = bpes.Today
+            .[Lupd_Prog] = bpes.ScrnNbr
+            .[Lupd_User] = bpes.UserId
+            .[MapLocation] = " "
+            .[MetodoPago] = " "
+            .[MiscAcct] = " "
+            .[MiscSub] = " "
+            .[Name] = " "
+            .[Nombre] = " "
+            .[NoteId] = 0
+            .[Numero] = " "
+            .[Phone] = " "
+            .[RangoSocial] = " "
+            .[RazonSocial] = " "
+            .[s4Future01] = " "
+            .[s4future02] = " "
+            .[s4Future03] = 0
+            .[s4Future04] = 0
+            .[s4Future05] = 0
+            .[s4Future06] = 0
+            .[s4Future07] = 0
+            .[s4Future08] = 0
+            .[s4Future09] = 0
+            .[s4Future10] = 0
+            .[s4future11] = " "
+            .[s4Future12] = " "
+            .[Salut] = " "
+            .[Sexo] = " "
+            .[ShipToId] = " "
+            .[ShipViaId] = " "
+            .[SiteId] = " "
+            .[SlsAcct] = " "
+            .[SlsPerId] = " "
+            .[SlsSub] = " "
+            .[State] = " "
+            .[Status] = " "
+            .[TaxId00] = " "
+            .[TaxId01] = " "
+            .[TaxId02] = " "
+            .[TaxId03] = " "
+            .[TaxLocId] = " "
+            .[TaxRegNbr] = " "
+            .[TelefonoCasa] = " "
+            .[TelefonoCelular] = " "
+            .[TelefonoOficina] = " "
+            .[TipoPersona] = " "
+            .[User1] = " "
+            .[User2] = " "
+            .[User3] = 0
+            .[User4] = 0
+            .[User5] = " "
+            .[User6] = " "
+            .[User7] = 0
+            .[User8] = 0
+            .[Zip] = " "
+
+        End With
 
     End Sub
 
